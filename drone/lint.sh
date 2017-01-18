727e6a3cd7897efe7d2570485bdad77472cc6c08
gudmojo <gudmundur.orn@gmail.com>
2017-01-18 20:32:55 +0000

    More advanced parallelism, dispatch to scripts

---
+++ drone/lint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+set -u
+set -x
+SHARD=$1
+echo "Running lint"
+sleep 30
---
+++ drone/system-test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+set -u
+set -x
+SHARD=$1
+echo "Running system test for shard $SHARD"
+sleep 30
